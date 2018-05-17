05552b7a2c0ff308d9a017ad1a9fccf56f828bcf
 TheSmartnik <misharinn@gmail.com>
2018-05-17 14:35:14 +0300

    Improve structure

--- sketch.js
+++ sketch.js
@@ -1,14 +1,31 @@
+var numberOfRows = 3;
+var numberOfColumns = 2;
+
 function setup() {
   createCanvas(400, 560);
   noStroke();
-  // background(240, 120, 0);
-  
+  drawGrid();
+}
+
+function draw() {
+}
+
+blockWidth = function() {
+  return width / numberOfColumns;
+}
+
+blockHeight = function() {
+  return height / numberOfRows;
+}
+
+drawGrid = function() {
   currentOffsetX = 0;
   currentOffsetY = 0;
   colors = ['#ff7800', '#37be8c', '#cda500', '#5a87e1', '#d7e6e1'];
+  // colors = ['#1D35D6', '#FFD40E', '#FFFEF8','#FFFEF8','#FFFEF8', '#FF2F20', '#000000'];
   
-  for(var row = 0; row < 3; row++) {
-    for(var column = 0; column < 2; column++) {
+  for(var row = 0; row < numberOfRows; row++) {
+    for(var column = 0; column < numberOfColumns; column++) {
       fill(random(colors));
       rect(currentOffsetX, currentOffsetY, blockWidth(), blockHeight());
       currentOffsetX += blockWidth();
@@ -16,15 +33,4 @@ function setup() {
     currentOffsetX = 0;
     currentOffsetY += blockHeight();
   }
-}
-
-function draw() {
-}
-
-blockWidth = function() {
-  return width / 2;
-}
-
-blockHeight = function() {
-  return height / 3;
 }
